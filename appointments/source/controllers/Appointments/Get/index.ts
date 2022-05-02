6fcd3ed5ae53686d077772906ada9da110a44e6c
Fernando De La Torre <ref2k1@gmail.com>
2022-05-02 13:10:36 -0500

    Pending add

--- appointments/source/controllers/Appointments/Get/index.ts
+++ appointments/source/controllers/Appointments/Get/index.ts
@@ -1,1 +1,120 @@
 export const getController = () => {};
+import { Request, Response } from "express";
+import db from "../../../db/db";
+
+enum EStatus {
+  PENDING = "PENDING",
+  ACCEPTED = "ACCEPTED",
+  COMPLETED = "COMPLETED",
+  CANCELED = "CANCELED",
+}
+interface IAppointmentData extends IAppointmentDataMod {
+  id: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface IAppointmentDataMod {
+  date?: string | Date | Date;
+  id_subject?: string;
+  status?: EStatus;
+  location?: string;
+  problem_description?: string;
+  photo_url?: string;
+}
+
+interface IIdsAppointmentData extends IIdsAppointmentDataMod {
+  id: string;
+  id_appointment: string;
+}
+
+interface IIdsAppointmentDataMod {
+  id_student?: string;
+  id_advisor?: string;
+  id_admin?: string;
+}
+
+interface IGetAppointment {
+  id: string;
+  id_type?: "admin" | "student" | "advisor";
+}
+
+// ---------------------- PRIMERA -------------------------------------------
+// Endpoint que obtiene la asesoría más reciente de un admin
+
+export const getAdmin = async (req: Request, res: Response) => {
+  console.log("GET :D ADMIN FUNCIONA");
+  const id = req.query["id"];
+
+  try {
+    const adminFirstAppointment: any = await db
+      .first("*")
+      .from("appointments")
+      .where("id", id as string)
+      .orderBy("created_at", "desc");
+    res.json(adminFirstAppointment);
+    console.log(adminFirstAppointment);
+    res.statusCode = 200;
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+// ---------------------- SEGUNDA -------------------------------------------
+// Endpoint que obtiene la asesoría activa más reciente de un admin
+
+export const getStatus = async (req: Request, res: Response) => {
+  console.log("GET :D ESTATUS FUNCIONA");
+  const id = req.query["id"];
+
+  try {
+    const adminFirstAppointment: any = await db
+      .first("*")
+      .from("appointments")
+      .join(
+        "appointments-user",
+        "appointments.id",
+        "=",
+        "appointments-user.id_appointment"
+      )
+      .where({ status: "PENDING", "appointments-user.id_admin": id })
+      .orderBy("appointments.created_at", "desc");
+    res.json(adminFirstAppointment);
+    res.statusCode = 200;
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+// ---------------------- TERCERA -------------------------------------------
+// Todas las asesorías dependiendo del tipo de usuario
+
+export const getAll = async (req: Request, res: Response) => {
+  console.log("GET :D TODO FUNCIONA");
+  const { id, id_type } = req.query;
+  let columna: string;
+  if (id_type == "admin") {
+    columna = "appointments-user.id_admin";
+  } else if (id_type == "advisor") {
+    columna = "appointments-user.id_advisor";
+  } else {
+    columna = "appointments-user.id_student";
+  }
+
+  try {
+    const adminFirstAppointment: any = await db
+      .select("*")
+      .from("appointments")
+      .join(
+        "appointments-user",
+        "appointments.id",
+        "=",
+        "appointments-user.id_appointment"
+      )
+      .where(columna, id as string);
+    res.json(adminFirstAppointment);
+    res.statusCode = 200;
+  } catch (error) {
+    res.send(error);
+  }
+};
