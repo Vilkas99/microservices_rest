534f12093892f858fdfc62de655c866637f8b4f2
Fernando De La Torre <ref2k1@gmail.com>
2022-05-02 12:16:04 -0500

    git ignore:c

--- appointments/source/routes/Appointments/index.ts
+++ appointments/source/routes/Appointments/index.ts
@@ -1,5 +1,10 @@
 import { updateController } from "../../controllers/Appointments/Update";
 import { createController } from "../../controllers/Appointments/Create";
+import {
+  getAdmin,
+  getStatus,
+  getAll,
+} from "../../controllers/Appointments/Get";
 
 const express = require("express");
 const router = express.Router();
@@ -9,5 +14,8 @@ router.get("/", () => {
 });
 router.post("/", createController);
 router.patch("/", updateController);
+router.get("/admin", getAdmin);
+router.get("/status", getStatus);
+router.get("/allAppointments", getAll);
 
 module.exports = router;
