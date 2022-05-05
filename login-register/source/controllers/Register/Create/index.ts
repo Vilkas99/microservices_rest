ffc6434c0fe4f56c72b3c5787f4d2b8a50799eb4
Alejandro López <alexlohe25@gmail.com>
2022-05-05 17:01:18 -0500

    full get user data package

--- login-register/source/controllers/Register/Create/index.ts
+++ login-register/source/controllers/Register/Create/index.ts
@@ -32,74 +32,84 @@ interface INewUserSchedule {
   period: Number;
 }
 export const createUser = async (req: Request, res: Response) => {
-  //Create user
-  const {
-    name,
-    email,
-    password,
-    career,
-    semester,
-    careerDD,
-    semesterDD,
-    status,
-    type,
-    schedules,
-  }: INewUserData = req.body;
   try {
-    let newUserId = uuid();
-    await db("users").insert({
-      id: newUserId,
-      name: name,
-      email: email,
-      password: password,
-      status: status,
-      type: type,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
-
-    //Insert user data in careers table if type is advisor or student
-    if (EType.student === type || EType.advisor === type) {
-      await db("users-career").insert({
-        id: uuid(),
-        id_user: newUserId,
-        id_career: career,
-        semester: semester,
+    //Create user
+    const {
+      name,
+      email,
+      password,
+      career,
+      semester,
+      careerDD,
+      semesterDD,
+      status,
+      type,
+      schedules,
+    }: INewUserData = req.body;
+    const verifyNewUserQuery = await db("users")
+      .where({ email: email })
+      .select("id");
+    if (verifyNewUserQuery.length == 0) {
+      let newUserId = uuid();
+      await db("users").insert({
+        id: newUserId,
+        name: name,
+        email: email,
+        password: password,
+        status: status,
+        type: type,
         created_at: new Date(),
         updated_at: new Date(),
       });
-      if (careerDD != undefined) {
+
+      //Insert user data in careers table if type is advisor or student
+      if (EType.student === type || EType.advisor === type) {
         await db("users-career").insert({
           id: uuid(),
           id_user: newUserId,
-          id_career: careerDD,
-          semester: semesterDD,
+          id_career: career,
+          semester: semester,
           created_at: new Date(),
           updated_at: new Date(),
         });
+        if (careerDD != undefined) {
+          await db("users-career").insert({
+            id: uuid(),
+            id_user: newUserId,
+            id_career: careerDD,
+            semester: semesterDD,
+            created_at: new Date(),
+            updated_at: new Date(),
+          });
+        }
       }
-    }
-    //Create user schedules if type is advisor
-    if (EType.advisor === type && schedules != undefined) {
-      for (let i = 0; i < schedules.length; i++) {
-        let newScheduleId = uuid();
-        await db("schedules").insert({
-          id: newScheduleId,
-          start: schedules[i].start,
-          finish: schedules[i].finish,
-          period: schedules[i].period,
-        });
-        await db("users-schedule").insert({
-          id: uuid(),
-          id_user: newUserId,
-          id_schedule: newScheduleId,
-        });
+      //Create user schedules if type is advisor
+      if (EType.advisor === type && schedules != undefined) {
+        for (let i = 0; i < schedules.length; i++) {
+          let newScheduleId = uuid();
+          await db("schedules").insert({
+            id: newScheduleId,
+            start: schedules[i].start,
+            finish: schedules[i].finish,
+            period: schedules[i].period,
+          });
+          await db("users-schedule").insert({
+            id: uuid(),
+            id_user: newUserId,
+            id_schedule: newScheduleId,
+          });
+        }
       }
+      res.json({
+        status: "OK",
+        userId: newUserId,
+      });
+    } else {
+      res.json({
+        status: "ERROR",
+        msg: "User already created with email: " + email,
+      });
     }
-    res.json({
-      status: "OK",
-      userId: newUserId,
-    });
   } catch (error) {
     console.log("ERROR AL CREAR USUARIO");
     res.send(error);
--- login-register/source/controllers/Register/Get/index.ts
+++ login-register/source/controllers/Register/Get/index.ts
@@ -5,13 +5,31 @@ export const getUserData = async (req: Request, res: Response) => {
   const { id } = req.query;
 
   try {
-    let userQuery: any;
-    userQuery = await db("users").where({ id: id });
+    //get user basic data
+    const userQuery = await db("users").where({ id: id });
     let userData = userQuery[0];
-    res.json({
-      status: "OK",
-      user: userData,
-    });
+    //get user career data
+    const userCareerQuery = await db("users-career")
+      .where({ id_user: id })
+      .select("id_career", "semester");
+    //get user schedule data if their user type is advisor
+    if (userData.type === "advisor") {
+      const userScheduleQuery = await db("users-schedule")
+        .where({ id_user: id })
+        .select("id_schedule");
+      res.json({
+        status: "OK",
+        user: userData,
+        userCareer: userCareerQuery,
+        userSChedules: userScheduleQuery,
+      });
+    } else {
+      res.json({
+        status: "OK",
+        user: userData,
+        userCareer: userCareerQuery,
+      });
+    }
   } catch (error) {
     res.send(error);
   }
