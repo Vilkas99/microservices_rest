121eb32877c5a096d39538851ed49abfeca06b4f
vilkas99 <victorium99@hotmail.com>
2022-05-26 00:22:34 -0500

    Updater Schedule completed

--- dashboard/source/models/Schedules.ts
+++ dashboard/source/models/Schedules.ts
@@ -4,31 +4,6 @@ class SchedulesModel extends Model {
   static get tableName() {
     return "schedules";
   }
-
-  //TODO: Cambiar todo este método.
-  static get relationMappings(): RelationMappings | RelationMappingsThunk {
-    const Consultant = require("./Consultant");
-    const Debt = require("./Debt");
-
-    return {
-      consultants: {
-        relation: Model.HasManyRelation,
-        modelClass: Consultant,
-        join: {
-          from: "administrators.id",
-          to: "consultants.id_admin",
-        },
-      },
-      debts: {
-        relation: Model.HasManyRelation,
-        modelClass: Debt,
-        join: {
-          from: "administrators.id",
-          to: "debts.id_admin",
-        },
-      },
-    };
-  }
 }
 
 module.exports = SchedulesModel;
--- dashboard/source/server.ts
+++ dashboard/source/server.ts
@@ -32,6 +32,7 @@ exports.io = io;
 
 //Routes
 const notification_routes = require("./routes/notifications");
+const schedules_routes = require("./routes/schedules");
 /*
 const example_routes = require("./routes/Example");
 const appointment_routes = require("./routes/Appointments");
@@ -45,6 +46,7 @@ router.use(express.json());
 router.use(cors());
 //Apply routes
 router.use("/notification", notification_routes);
+router.use("/schedule", schedules_routes);
 /*
 router.use("/admin", example_routes);
 router.use("/appointment", appointment_routes);
