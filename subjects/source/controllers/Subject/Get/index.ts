32186ef15739c52b8e10d36e2bf5ce4e1c87b222
myrosandrade <myrosandrade89@gmail.com>
2022-06-03 18:09:52 -0500

    Get endpoints subjects finished

--- subjects/source/controllers/Subject/Get/index.ts
+++ subjects/source/controllers/Subject/Get/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import db from "../../../db/db";
 
 const SubjectModel = require("../../../models/Subjects");
 const SubjectCareerModel = require("../../../models/SubjectCareer");
@@ -52,30 +51,49 @@ export const getSubjectCareerController = async (
     return;
   }
 
-  db.raw(
-    `SELECT subjects.id, subjects.name, subjects.acronym FROM "career-subject" INNER JOIN subjects ON 
-    "career-subject".id_subject = subjects.id
-    WHERE "career-subject".id_career = '${idCarrera}'
-    AND subjects.semester <= '${semester}'
-    OR "career-subject".id_career = 'b2a9a7c6-cc4f-4507-b70f-6bdb7488e748'`
-  ).then((resp) => {
-    if (resp.rows === undefined || resp.rows.length === 0) {
-      res
-        .status(404)
-        .send("Error: Subjects for career and semester not found.");
-      return;
-    }
-    res.status(200);
-    res.json(resp.rows);
-  });
-
   try {
+    const careerSubject: any = await SubjectModel.query()
+      .select("subjects.id", "subjects.name", "subjects.acronym")
+      .from("career-subject")
+      .innerJoin("subjects", "career-subject.id_subject", "subjects.id")
+      .where("career-subject.id_career", idCarrera)
+      .andWhere("subjects.semester", "<=", semester)
+      .orWhere(
+        "career-subject.id_career",
+        "b2a9a7c6-cc4f-4507-b70f-6bdb7488e748"
+      );
+
+    res.json(careerSubject);
+    res.statusCode = 200;
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
-export const getAllSubjectsController = async (
-  req: Request,
-  res: Response
-) => {};
+export const getAllSubjectsController = async (req: Request, res: Response) => {
+  const { page, limitItems } = req.query;
+  let off = 0;
+  if (page && limitItems) {
+    off = (+page - 1) * +limitItems;
+  }
+
+  try {
+    const subjects: any = await SubjectModel.query()
+      .select(
+        "subjects.acronym as subjectAcronym",
+        "subjects.name",
+        "careers.acronym as careerAcronym",
+        "subjects.semester"
+      )
+      .from("career-subject")
+      .innerJoin("subjects", "career-subject.id_subject", "subjects.id")
+      .innerJoin("careers", "career-subject.id_career", "careers.id")
+      .limit(limitItems)
+      .offset(off);
+
+    res.json(subjects);
+    res.statusCode = 200;
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- subjects/source/routes/Subject/index.ts
+++ subjects/source/routes/Subject/index.ts
@@ -1,9 +1,11 @@
 import { getSubjectCareerController } from "../../controllers/Subject/Get";
+import { getAllSubjectsController } from "../../controllers/Subject/Get";
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/career", getSubjectCareerController);
+router.get("/", getAllSubjectsController);
 // router.post("/", createSchedule);
 // router.patch("/", updateSchedule);
 
