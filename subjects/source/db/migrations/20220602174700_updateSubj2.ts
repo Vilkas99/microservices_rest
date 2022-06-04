593d9a37f43ed9db46ded19ee29684b9c63b7ff1
myrosandrade <myrosandrade89@gmail.com>
2022-06-03 22:59:59 -0500

    migration deleted

--- subjects/source/db/migrations/20220602174700_updateSubj2.ts
+++
@@ -1,9 +0,0 @@
-import Knex = require("knex");
-
-export async function up(knex: Knex): Promise<void> {
-  return knex.schema.alterTable("subjects", (table) => {
-    table.boolean("optative");
-  });
-}
-
-export async function down(knex: Knex): Promise<void> {}
