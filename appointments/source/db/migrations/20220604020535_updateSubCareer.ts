117e0f216f3ebd90e1bc4d0c930c99b68ec302c2
UzielLopez <61522818+UzielLopez@users.noreply.github.com>
2022-06-05 01:50:17 -0500

    Updated migrations

--- appointments/source/db/migrations/20220604020535_updateSubCareer.ts
+++ appointments/source/db/migrations/20220604020535_updateSubCareer.ts
@@ -1,9 +1,13 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return;
+  return knex.schema.alterTable("career-subject", (table) => {
+    table.integer("semester");
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return;
+  return knex.schema.alterTable("subjects", (table) => {
+    table.dropColumn("semester");
+  });
 }
